32bfd35d948506e9b0b4d063f61e360c50de023a
Andrew Shipilo <superdron97@yandex.ru>
2018-03-11 22:53:54 +0300

    GOST without randomization and gmp c bindings

---
+++ Signature.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andrew on 11.03.18.
+//
+
+#include "Signature.h"
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "md5.h"
+#include "Signature.h"
+
+int main()
+{
+    std::string hash = md5file("file");
+    mpz_t hash_t;
+
+    mpz_init_set_str(hash_t, hash.c_str(), 16);
+
+    std::cout << hash << std::endl;
+
+    Signature sig(hash);
+    sig.attributes.generate();
+    sig.make();
+    sig.writeToFile("file.sg");
+
+    Signature fileSig;
+    fileSig.readFromFile("file.sg");
+    fileSig.verify(sig.attributes.p, sig.attributes.q, sig.attributes.a, sig.attributes.y, hash_t);
+
+    return 0;
+}
